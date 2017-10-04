9da1489f5ebfba0a34f496099bb15449cdf85570
Dayong Sun <dsun29@gmail.com>
2017-10-04 10:24:33 -0400

    firewall enable public

--- installnginx.sh
+++ installnginx.sh
@@ -2,3 +2,6 @@ sudo yum install epel-release
 sudo yum install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
